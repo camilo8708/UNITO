36e52cdbddf820464be0275a6647d5667ef3e5f0
kleer <kleer@kleer-csd-box>
2017-12-01 09:01:31 -0500

    Creacion test con cucumber

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -1,1 +1,24 @@
 
+#You can implement step definitions for undefined steps with these snippets:
+
+Given(/^que inicie la aplicacion$/) do
+  visit '/' 
+end
+
+Then(/^debo ver titulo "([^"]*)"$/) do |jugador|
+  expect(page.body).to match /#{jugador}/m
+end
+
+Then(/^debo ver una carta principal "([^"]*)" color "([^"]*)"$/) do |numero, color|
+  expect(page.body).to match /#{color}/m
+  expect(page.body).to match /#{numero}/m
+
+end
+
+Then(/^debo ver turno "([^"]*)"$/) do |turno|
+  expect(page.body).to match /#{turno}/m
+end
+
+#Then(/^debo ver "([^"]*)" cartas para jugador "([^"]*)"$/) do |arg1, arg2|
+  
+#end
