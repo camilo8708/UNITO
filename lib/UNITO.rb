8ebf19e3d20f362b66ed2e15267d6b33962abfc9
kleer <kleer@kleer-csd-box>
2017-12-01 09:29:32 -0500

    Inicio juego

---
+++ lib/UNITO.rb
@@ -0,0 +1,14 @@
+class UNITO
+	color = ['GREEN', 'YELLOW', 'RED', 'BLUE']
+	numeros = [1,2,3,4,5,6,7]
+	@cards
+	@cardGame
+
+	def cards jugador
+		@cards = 1
+	end
+
+	def cardGame
+		@cardGame = 1
+	end
+end
