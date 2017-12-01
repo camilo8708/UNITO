b4db43c8de58da575522d7a9e15a7bc12779ce9c
kleer <kleer@kleer-csd-box>
2017-12-01 09:51:22 -0500

    Controlador validacion ganador

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,11 +2,25 @@ require './lib/UNITO'
 describe UNITO do
     it "inicia Juego y devolver carta" do
         unito = UNITO.new
-        expect(unito.cards 1).to eq (1 or 2 or 3 or 4 or 5 or 6 or 7)
+        expect(unito.cards 1,6).to eq 6
     end
 
 	it "inicia Juego y devolver carta en juego" do
         unito = UNITO.new
-        expect(unito.cardGame).to eq (1 or 2 or 3 or 4 or 5 or 6 or 7)
+        expect(unito.cardGame 5).to eq 5
     end    
+
+    it "inicia Juego y valido si gańo" do
+        unito = UNITO.new
+        expect(unito.cards 1,6).to eq 6
+        expect(unito.cardGame 5).to eq 5
+        expect(unito.status).to eq "Perdio"
+    end
+
+    it "inicia Juego y valido si gańo" do
+        unito = UNITO.new
+        unito.cards 1,7
+        unito.cardGame 7
+        expect(unito.status).to eq "Gano"
+    end
 end
